4e22483681c7da88a373c760fe230f1bfc7a2c53
ekesto <beat@ekesto.com>
2017-08-04 13:24:03 +0200

    bootstrap select dynamic header offst fix

--- js/modules/bootstrap-select.js
+++ js/modules/bootstrap-select.js
@@ -12,10 +12,13 @@ import 'bootstrap-select/dist/js/bootstrap-select';
 $(function(){
 
     function initBootstrapSelect() {
+        // dynamically define offset of select because of fixed site header
+        let bs_select_offset_top = $('.site-header').outerHeight();
         $('.selectpicker').selectpicker({
           style: 'btn-default bs-placeholder',
           size: 'auto',
           dropupAuto: true, // per default, let bootstrap select choose where to dislpay the dropdown (above or below). Should you want to force to display the form below, add the `data-dropup-auto="false"` attribute to the desired <select> element.
+          windowPadding: [bs_select_offset_top,0,0,0],
         });
     }
 
