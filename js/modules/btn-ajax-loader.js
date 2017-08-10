1c2dca78b6177d2421926a1611245e0e01d6fe69
ekesto <beat@ekesto.com>
2017-08-10 14:51:23 +0200

    btn ajax sizing issue fixed

--- js/modules/btn-ajax-loader.js
+++ js/modules/btn-ajax-loader.js
@@ -26,6 +26,18 @@ function setBtnAjaxLoader($btn) {
     $btn.addClass('loading');
 }
 
+function setBtnAjaxLoaderSize($btn) {
+    // set dimensions...
+    var width = $btn.outerWidth();
+    var height = $btn.outerHeight();
+    // ...but only if the item is visible (in case the element is within an elemnt with display:none, we get 0)
+    if (width > 0) {
+        $btn
+            .css('width',width)
+            .css('height',height);
+    }
+}
+
 function initBtnAjaxLoader() {
     // find buttons and loop!
     var btn_elements = document.querySelectorAll('[data-btn-ajax-loader]');
@@ -33,15 +45,7 @@ function initBtnAjaxLoader() {
         for (var i = 0; i < btn_elements.length; i++) {
             // init
             var $element = $(btn_elements[i]);
-            // set dimensions...
-            var width = $element.outerWidth();
-            var height = $element.outerHeight();
-            // ...but only if the item is visible (in case the element is within an elemnt with display:none, we get 0)
-            if (width > 0) {
-                $element
-                    .css('width',width)
-                    .css('height',height);
-            }
+            var initialized_attr = 'data-trigger-initialized';
             // listen for click event
             $element.on('click', function(e){
                 // init
@@ -52,15 +56,16 @@ function initBtnAjaxLoader() {
                     // required mini-delay to check if the class has been added
                     setTimeout(function(){
                         if($btn.parents('form').hasClass('has-errors') === false) {
+                            setBtnAjaxLoaderSize($btn)
                             setBtnAjaxLoader($btn);
                         }
                     },10);
                 }
                 // otherwise, let it load right away!
                 else {
+                    setBtnAjaxLoaderSize($btn);
                     setBtnAjaxLoader($btn);
                 }
-
             });
         }
     }
@@ -95,30 +100,22 @@ function resetBtnAjaxLoaderSize() {
 
 $(function(){
 
-    // on page load (delayed, because there was an issue when the page wasn't rendered properly and the button size was set wrong)
-    // But it doesn't matter anyway, no need to rush, this can happen in the background
-    setTimeout(function(){
-        initBtnAjaxLoader();
-    },1500);
+    // on page load
+    initBtnAjaxLoader();
 
     // custom event
-    $(window).on('initBtnAjaxLoader', function() {
+    $(window).on('initBtnAjaxLoader softpage:opened', function() {
         initBtnAjaxLoader();
     });
 
     // custom event
-    $(window).on('btnAjaxLoaderDone', function() {
+    $(window).on('btnAjaxLoaderDone ajaxLoadItems:success', function() {
         btnAjaxLoaderDone();
     });
 
     // listen for a viewport width change and re-init the loaders (because of resizing the)
     $(window).on('viewportWidthHasChanged', function(){
-        // reset sizes
         resetBtnAjaxLoaderSize();
-        // and delayed: set sizes anew
-        setTimeout(function(){
-            initBtnAjaxLoader();
-        },150);
     });
 
 });
