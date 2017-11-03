1d5b872d995b5b2e2a7cdd3f835f976bc141bb17
ekesto <beat@ekesto.com>
2017-11-03 11:25:35 +0100

    bootstrap select markup cleanup on init

--- js/modules/bootstrap-select.js
+++ js/modules/bootstrap-select.js
@@ -15,11 +15,17 @@ $(function(){
         // dynamically define offset of select because of fixed site header
         let bs_select_offset_top = $('.site-header').outerHeight();
         $('.selectpicker').selectpicker({
-          style: 'btn-default bs-placeholder',
-          size: 'auto',
-          dropupAuto: true, // per default, let bootstrap select choose where to dislpay the dropdown (above or below). Should you want to force to display the form below, add the `data-dropup-auto="false"` attribute to the desired <select> element.
-          windowPadding: [bs_select_offset_top,0,0,0],
+            style: 'btn-default bs-placeholder',
+            size: 'auto',
+            dropupAuto: true, // per default, let bootstrap select choose where to dislpay the dropdown (above or below). Should you want to force to display the form below, add the `data-dropup-auto="false"` attribute to the desired <select> element.
+            windowPadding: [bs_select_offset_top,0,0,0],
         });
+        setTimeout(function(){
+            $('.bootstrap-select .dropdown-toggle').each(function(){
+                this.innerHTML = this.innerHTML.replace(/&nbsp;/g,'');
+                $(this).find('.bs-caret').remove();
+            });
+        },0);
     }
 
     // on page load
