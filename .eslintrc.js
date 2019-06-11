606bbd68116f5360ed995baf6b80949a1a7850fe
Marius Küng <maku@mailbox.org>
2019-06-11 16:17:34 +0200

    Added eslint, updated editor-config, updated package.json

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        jquery: true,
+    },
+    extends: [
+        'plugin:vue/recommended',
+        'airbnb-base'
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+        CMS: true,
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: [
+        'vue',
+    ],
+    rules: {
+        "indent": ["error", 4],
+        "max-len": 0,
+        "vue/html-indent": ["error", 4],
+        "vue/max-attributes-per-line": 0,
+    },
+};
