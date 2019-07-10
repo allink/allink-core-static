9e934c08a6b28b682708fbcaf384f308aa528f18
Haldner Claudio <haldner@allink.ch>
2019-07-10 13:35:26 +0200

    new module seo_accordion

---
+++ js/modules/seo-accordion.js
@@ -0,0 +1,32 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+SEO Accordion
+
+*/
+
+export default function initSEOAccordion(options) {
+    const $seoAccordionItems = $('.seo-accordion__item');
+    const animationDuration = options.duration || 400;
+
+    $seoAccordionItems.each(function () {
+        const $item = $(this);
+        const $seoAccordionTitle = $item.children('.seo-accordion__title');
+        let animationRunning = false;
+
+        $seoAccordionTitle.on('click', function (event) {
+            event.preventDefault();
+
+            // prevent item to keep opening & closing when user clicks multiple times on title
+            if (animationRunning) {
+                return;
+            }
+
+            const $seoAccordionContent = $item.children('.seo-accordion__content');
+            $item.toggleClass('seo-accordion__item--open');
+            animationRunning = true;
+            $seoAccordionContent.slideToggle(animationDuration, 'swing', function () {
+                animationRunning = false;
+            });
+        });
+    });
+}
