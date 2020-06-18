f17e009d334b9819fd29adb7a1985d2c3d743f3f
Sergio Lavanga <lavanga@allink.ch>
2020-06-18 13:16:26 +0200

    Init accordions only once

--- js/modules/seo-accordion.js
+++ js/modules/seo-accordion.js
@@ -8,25 +8,29 @@ export default function initSEOAccordion(options) {
     const $seoAccordionItems = $('.seo-accordion__item');
     const animationDuration = options.duration || 400;
 
-    $seoAccordionItems.each(function () {
-        const $item = $(this);
-        const $seoAccordionTitle = $item.children('.seo-accordion__title');
-        let animationRunning = false;
+    if (typeof $seoAccordionItems.data('trigger-initialized') === 'undefined') {
+        $seoAccordionItems.each(function () {
+            const $item = $(this);
+            const $seoAccordionTitle = $item.children('.seo-accordion__title');
+            let animationRunning = false;
 
-        $seoAccordionTitle.on('click', function (event) {
-            event.preventDefault();
+            $seoAccordionTitle.on('click', function (event) {
+                event.preventDefault();
 
-            // prevent item to keep opening & closing when user clicks multiple times on title
-            if (animationRunning) {
-                return;
-            }
+                // prevent item to keep opening & closing when user clicks multiple times on title
+                if (animationRunning) {
+                    return;
+                }
 
-            const $seoAccordionContent = $item.children('.seo-accordion__content');
-            $item.toggleClass('seo-accordion__item--open');
-            animationRunning = true;
-            $seoAccordionContent.slideToggle(animationDuration, 'swing', function () {
-                animationRunning = false;
+                const $seoAccordionContent = $item.children('.seo-accordion__content');
+                $item.toggleClass('seo-accordion__item--open');
+                animationRunning = true;
+                $seoAccordionContent.slideToggle(animationDuration, 'swing', function () {
+                    animationRunning = false;
+                });
             });
         });
-    });
+
+        $seoAccordionItems.attr('data-trigger-initialized', '');
+    }
 }
