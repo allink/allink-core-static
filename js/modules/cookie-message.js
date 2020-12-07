2c484e9ea72bacf4b5821146cbfa62409fc01d53
Sergio Lavanga <lavanga@allink.ch>
2020-12-07 10:11:57 +0100

    Remove cookie message from DOM after animation

--- js/modules/cookie-message.js
+++ js/modules/cookie-message.js
@@ -24,5 +24,15 @@ export function initCookieMessage(options) {
             storage.setItem('isCookieAccepted', '1');
             messageBox.classList.add('cookie-message--hidden');
         });
+
+        // remove cookie message and spacer markup from DOM after the animation
+        messageBox.addEventListener('transitionend', (e) => {
+            if (messageBox.classList.contains('cookie-message--hidden') && e.propertyName === 'transform') {
+                const spacer = messageBox.previousElementSibling;
+
+                spacer.parentNode.removeChild(spacer);
+                messageBox.parentNode.removeChild(messageBox);
+            }
+        });
     }
 }
